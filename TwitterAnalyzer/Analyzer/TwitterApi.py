c02424b50cbd19f0a24e5fc753092676dca4b791
GrzegorzKrug <50370928+GrzegorzKrug@users.noreply.github.com>
2019-10-25 03:07:55 +0200

    Verify tokens

--- TwitterAnalyzer/Analyzer/TwitterApi.py
+++ TwitterAnalyzer/Analyzer/TwitterApi.py
@@ -9,23 +9,23 @@ import os
 
 class TwitterApi:
     def __init__(self, autologin=True):
-        self._overrider(False)
-        self.logged_in = False
+        # self._overrider(False)
         self.apiUrl = r'https://api.twitter.com/1.1/'
+        self.logged_in = False
+        self.auth = None
+
         self.following = None
         self.me = None
-        self._verifyOAuth()
+
         if autologin:
-            valid, text = self._verifyOAuth()
+            valid, text = self._login_procedure()
             print(text)
             if valid:
                 self.logged_in = True
-            # self.request_me()
 
     def _login_procedure(self):
-        self.logged_in, self.api, message = self._autologin_from_file()
+        self.logged_in, self.me, message = self._verifyOAuth()
         if self.logged_in:
-            self.me = self.api.VerifyCredentials()
             text = 'Logged in succesfuly as {}.'.format(self.me['screen_name'])
             return True, text
         else:
@@ -52,9 +52,11 @@ class TwitterApi:
                 try:
                     self.verify_response(ressponse.status_code)
                     message = 'Logged in succesfuly'
-                    return True, message
+                    self.auth = auth
+
+                    return True, ressponse.json(), message
                 except Unauthorized:
-                    return False, 'Authorization failed! Invalid or expired token.'
+                    return False, None, 'Authorization failed! Invalid or expired token.'
 
         except json.decoder.JSONDecodeError:
             print("secret_token.txt is not in json format!")
@@ -74,7 +76,8 @@ class TwitterApi:
         #
         # return True, api, message
 
-    def verify_response(self, resp_code):
+    @staticmethod
+    def verify_response(resp_code):
         if resp_code == 200:
             return True
         # if resp_code == 400:
@@ -90,55 +93,56 @@ class TwitterApi:
         else:
             raise Exception(f'Error, Response code {resp_code}')
 
-    def CollectHome(self, count=200):
-        try:
-            home = self.api.GetHomeTimeline(count=count)
-            return home
+    # def CollectHome(self, count=200):
+    #     try:
+    #         home = self.api.GetHomeTimeline(count=count)
+    #         return home
+    #
+    #     except AttributeError:
+    #         raise TwitterLoginFailed("Error! Login status: {}".format(self.logged_in))
 
-        except AttributeError:
-            raise TwitterLoginFailed("Error! Login status: {}".format(self.logged_in))
-
-    @staticmethod
-    def _overrider(display=True):
-        text = []
-
-        def add_items_method(self):
-            return self.__dict__.items()
-
-        def __getitem__(cls, x, missing=None):
-            return getattr(cls, x, missing)
-
-        # @classmethod
-        # def __getitem_class__(cls, x):
-        #     return getattr(cls, x)
-
-        twitter.User.items = add_items_method
-        text += ["New Method twitter.User.items"]
-
-        twitter.User.__getitem__ = __getitem__
-        text += ["User is subscriptable twitter.User.__getitem__"]
-
-        twitter.Status.items = add_items_method
-        text += ["New Method twitter.Status.items"]
-
-        twitter.Status.__getitem__ = __getitem__
-        text += ["Status is subscriptable twitter.Status.__getitem__"]
-
-        twitter.Status.get = __getitem__
-        text += ["New Method twitter.Status.get"]
-
-        if display:
-            for comment in text:
-                print('\t', comment)
-            print('#' * 20, 'End of overloading.', '#' * 20, '\n')
+    # @staticmethod
+    # def _overrider(display=True):
+    #     text = []
+    #
+    #     def add_items_method(self):
+    #         return self.__dict__.items()
+    #
+    #     def __getitem__(cls, x, missing=None):
+    #         return getattr(cls, x, missing)
+    #
+    #     # @classmethod
+    #     # def __getitem_class__(cls, x):
+    #     #     return getattr(cls, x)
+    #
+    #     twitter.User.items = add_items_method
+    #     text += ["New Method twitter.User.items"]
+    #
+    #     twitter.User.__getitem__ = __getitem__
+    #     text += ["User is subscriptable twitter.User.__getitem__"]
+    #
+    #     twitter.Status.items = add_items_method
+    #     text += ["New Method twitter.Status.items"]
+    #
+    #     twitter.Status.__getitem__ = __getitem__
+    #     text += ["Status is subscriptable twitter.Status.__getitem__"]
+    #
+    #     twitter.Status.get = __getitem__
+    #     text += ["New Method twitter.Status.get"]
+    #
+    #     if display:
+    #         for comment in text:
+    #             print('\t', comment)
+    #         print('#' * 20, 'End of overloading.', '#' * 20, '\n')
 
 
-class TooManyRequests(Exception):  # 429
+class Unauthorized(Exception):  # 401
     """Base class for Twitter errors"""
     @property
     def message(self):
         '''Returns the first argument used to construct this error.'''
         return self.args[0]
+
 
 class ApiNotFound(Exception):  # 404
     """Base class for Twitter errors"""
@@ -147,7 +151,8 @@ class ApiNotFound(Exception):  # 404
         '''Returns the first argument used to construct this error.'''
         return self.args[0]
 
-class Unauthorized(Exception):  # 401
+
+class TooManyRequests(Exception):  # 429
     """Base class for Twitter errors"""
     @property
     def message(self):
