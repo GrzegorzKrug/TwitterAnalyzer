87dcdf9cea6789db7df1dd71a3505fa655403116
GrzegorzKrug <50370928+GrzegorzKrug@users.noreply.github.com>
2019-10-30 17:27:23 +0100

    Setup.py

--- TwitterAnalyzer/setup.py
+++ TwitterAnalyzer/setup.py
@@ -1,14 +1,13 @@
 from setuptools import setup
 
 setup(
-   name='TwiterAnalyzer',
+   name='Analyzer',
    version='1.0',
    description='Gathering Tweets, Posting Tweets, Manipulation stored Tweets' \
    +' in CSV',
    author='Grzegorz Krug',
    author_email='my mail',
-   packages=['TwiterAnalyzer'],  #same as name
+   packages=['Analyzer'],  #same as name
    install_requires=['pandas', 'json', 'glob', 'threading', 'PyQt5',
-                     'requests', 'requests_oauthlib', 'os', 'time',
-                     'traceback'], #external packages as dependencies
+                     'requests', 'requests_oauthlib', 'time'], #external packages as dependencies
 )
