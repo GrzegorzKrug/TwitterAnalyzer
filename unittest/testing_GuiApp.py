a3cae37c24263b8aa21860edf42042d5912bc56d
Greg <grzesiekk321@gmail.com>
2019-11-14 18:07:22 +0100

    fixed testing in ubuntu

--- unittest/testing_GuiApp.py
+++ unittest/testing_GuiApp.py
@@ -10,8 +10,9 @@ MainWindow = QtWidgets.QMainWindow()
 app = TwitterAnalyzerGUI(MainWindow, autologin=False)
 
 global tweet_dir
-tweet_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)),
+tweet_dir = os.path.join(os.path.dirname(os.path.abspath(os.path.dirname(__file__))),
                         'TwitterAnalyzer', 'tweets')
+
 
 class UnitTest(unittest.TestCase):
     def test_Login_Collect_Home(self):
@@ -28,7 +29,6 @@ class UnitTest(unittest.TestCase):
 
     def test_create_delete_csv(self):
         filepath = os.path.join(tweet_dir, 'unittest.csv')
-        print(filepath)
         app.export_tweet_to_database('tweets', None, 'unittest')
         self.assertTrue(os.path.exists(filepath))
 
