29edbd117616308a1d226d8254a6dfa5e60ad989
Grzegorz Krug <grzesiekk321@gmail.com>
2020-03-06 17:57:00 +0100

    requirements update

--- setup.py
+++ setup.py
@@ -9,5 +9,5 @@ setup(
    author_email='my mail',
    packages=['twitter_analyzer'],  #same as name
    install_requires=['pandas==1.0.1', 'PyQt5==5.14.1',
-                     'request==2019.4.13', 'requests_oauthlib==1.3.0', 'pytest==5.3.5'], #external packages as dependencies
+                     'request==2019.4.13', 'requests==2.23.0', 'requests_oauthlib==1.3.0', 'pytest==5.3.5'],
 )
