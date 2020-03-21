2d8bf7eb0522a3a0bfd9193db8abe530b573e338
Grzegorz Krug <grzesiekk321@gmail.com>
2020-03-21 17:40:17 +0100

    connection test is ok

--- twitter_analyzer/learnig_db.py
+++ twitter_analyzer/learnig_db.py
@@ -1,10 +1,20 @@
 from sqlalchemy import create_engine
 
-user = 'greg'
-password = 123
-host = 'host'
+dbname = 'postgres'
+user = 'admin'
+password = 'docker'
+
+host = '127.0.0.1'
 port = 5432
-dbname = 'dbname'
 url = f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{dbname}"
 
-engine = create_engine(url)
+engine = create_engine(url,
+                       connect_args={'client_encoding': 'utf8'}
+                       )
+
+
+with engine.connect() as connection:
+    pass
+    # result = connection.execute("select username from users")
+    # for row in result:
+    #     print("username:", row['username'])
