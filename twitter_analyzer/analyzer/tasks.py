fa64fd734b3e734dce541e1e2646fafe2af826a1
Grzegorz Krug <grzesiekk321@gmail.com>
2020-05-19 23:56:32 +0200

    save

--- twitter_analyzer/analyzer/tasks.py
+++ twitter_analyzer/analyzer/tasks.py
@@ -19,15 +19,19 @@ app = Celery('tasks', broker=broker_url)
 
 
 @app.task
-def show():
-    print("Hello celery 15")
-    for x in range(15):
-        print(15 - x)
-        time.sleep(1)
+def show(*args, **kwargs):
+    duration = 10
+    print(f"Start, args: {args}, kwargs: {kwargs}")
+    for x in range(duration):
+        print(duration - x)
+        time.sleep(0.3)
+
     print("Bye celery")
 
 
 if __name__ == "__main__":
     # app.control.autoscale(10, 4)
     # app.conf.autoscale(40)
+    # app.conf.worker_max_tasks_per_child = 10
+    # app.conf.worker_pool = 10
     app.worker_main()
