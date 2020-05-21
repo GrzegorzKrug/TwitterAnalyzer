1cf3966d6f1674ec1d5a71bdc83c3f774cb9dfaf
Grzegorz Krug <grzesiekk321@gmail.com>
2020-05-21 18:35:56 +0200

    save

---
+++ twitter_analyzer/analyzer/__init__.py
@@ -0,0 +1,5 @@
+from __future__ import absolute_import, unicode_literals
+
+from .my_celery_app import app
+
+__all__ = ('app',)
--- twitter_analyzer/analyzer/my_celery_app.py
+++ twitter_analyzer/analyzer/my_celery_app.py
@@ -1,39 +1,16 @@
 from __future__ import absolute_import, unicode_literals
-from celery.concurrency.prefork import TaskPool
-from celery.worker.autoscale import Autoscaler
 from celery import Celery
 
-import time
 import os
+
+# os.environ.setdefault('worker_pool', '10')
 
 user = os.getenv('LOGIN', 'admin')
 password = os.getenv('PASSWORD', 'mypass')
 hostname = os.getenv('HOSTNAME', 'localhost')
 
 broker_url = f'amqp://{user}:{password}@{hostname}:5672/'
-app = Celery('tasks', broker=broker_url, namespace="CELERY")
-
-
-# app.conf.worker_pool = TaskPool
-# app.control.p
-# app.control.worker_max_tasks_per_child = 10
-# app.conf.worker_autoscaler = Autoscaler(None, min_concurrency=3, max_concurrency=10)
-
-
-@app.task
-def show(*args, **kwargs):
-    duration = 10
-    print(f"Start, args: {args}, kwargs: {kwargs}")
-    for x in range(duration):
-        print(duration - x)
-        time.sleep(0.3)
-
-    print("Bye celery")
-
+app = Celery('tasks', broker=broker_url, namespace="celery", include=['tasks'])
 
 if __name__ == "__main__":
-    # app.control.autoscale(10, 4)
-    # app.conf.autoscale(40)
-    # app.conf.worker_max_tasks_per_child = 10
-    # app.conf.worker_pool = 10
-    app.worker_main()
+    app.worker_main(argv=["--autoscale", "10,3"])
